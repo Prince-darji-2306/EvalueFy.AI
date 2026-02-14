297e72be2c96999f7eb5507ac07520bd1049723d
Prince <princedarji716@gmail.com>
2026-02-14 14:55:06 +0530

    Some changes of optimization

--- agents/Evaluator.py
+++ agents/Evaluator.py
@@ -4,15 +4,17 @@ from llm_engine import get_llm
 def evaluate_response(question, answer):
     llm = get_llm()
     prompt = f"""
-    You are an expert interviewer. Evaluate the candidate's answer to the given question.
-    
+    You are an interviewer. Evaluate the candidate's answer to the given question. The Candidate is a Fresher. So, Do not be too strict.
+    Ignore all the grammatical errors, as well as number errors and spelling errors.
+    You are a human you have to understand by language.
+
     Question: {question}
     Answer: {answer}
     
     Provide your evaluation in STRICT JSON format with the following keys:
     - "score": An integer from 0 to 10.
     - "reason": A brief explanation of the score.
-    - "improvements": How the answer could be improved.
+    - "improvements": Explaining How the answer could be improved.
     - "follow_up": If the score is below 6, provide a relevant follow-up question to clarify or dive deeper. Otherwise, set this to null.
     
     Return ONLY the JSON.
--- llm_engine.py
+++ llm_engine.py
@@ -10,21 +10,3 @@ def get_llm():
         model_name="meta-llama/llama-4-scout-17b-16e-instruct",
         groq_api_key=os.getenv("GROQ_API_KEY")
     )
-
-def get_response(question, answer):
-    llm = get_llm()
-    prompt = f"""
-    You are a interviewer.
-    Question: {question}
-    Answer: {answer}
-    
-    Ignore all the grammatical errors, as well as number errors and spelling and number errors.
-    You are a human you have to understand by language.
-    Provide a very concise and short response in the following format:
-    SCORE: [0-10]
-    REASON: [Why this score was given]
-    IMPROVEMENTS: [How the answer could be better]
-    """
-    response = llm.invoke(prompt)
-    return response.content
-
